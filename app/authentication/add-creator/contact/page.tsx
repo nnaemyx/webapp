0805419a9aad1b46e5d267a0cbabd33f45bbccdf
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2024-11-07 08:19:53 +0100

    Added icons to repo

---
+++ app/authentication/add-creator/contact/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+import ContactDetails from '../_components/ContactDetails'
+const page = () => {
+  return (
+    <div>
+        <ContactDetails/>        
+    </div>
+  )
+}
+
+export default page
