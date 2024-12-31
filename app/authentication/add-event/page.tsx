e89df2fd8f4ebf04e63a00ef5da65ee65b336008
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2024-12-31 11:14:35 +0100

    added add tickets

--- app/authentication/add-event/page.tsx
+++ app/authentication/add-event/page.tsx
@@ -8,8 +8,9 @@ import EyeShowIcon from "@/components/icons/EyeShowIcon";
 import EyeHideIcon from "@/components/icons/EyeHideIcon";
 import sideImage from "/public/assests/signup image.png"
 import { useRouter } from "next/navigation";
-
-function ContactDetails() {
+import { callApi } from "@zayne-labs/callapi";
+import { toast } from "@/components/ui/use-toast";
+function EventCreatorLogin() {
 const router = useRouter()
 
   useEffect(() => {
@@ -19,16 +20,35 @@ const router = useRouter()
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false)
 
-  const nextpage = () =>{
-    router.push("/authentication/add-event/eventinfo")
-  }
-    const numbers = [1,2,3]
    const details = [
     {type: 'email', name: "Email Address", placeholder: "your email goes here", value: email, edit: (value: string)=>{setEmail(value)}},
     {type: showPassword ? 'text' : "password" , name: "Password", placeholder: "*******", value: password, edit: (value: string)=>{setPassword(value)}}
 ]
 const data = {email, password}
+
+const submit = async() =>{
+  setLoading(true)
+    await callApi<{message: string, accessToken:string} >( process.env.NEXT_PUBLIC_NEXT_ENV === "development" ? 'http://localhost:5000/api/creator/login' : `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/creator/login`,
+          {method: "POST", body:data,
+            onResponse: ({data})=>{
+             toast({
+               description: data.message
+             });
+             setLoading(false)
+             router.push("/authentication/add-event/eventinfo")
+           },
+           onError: ({ error})=>{     
+               toast({
+                 variant: "destructive",
+                 description: error.message,
+               });    
+               setLoading(false)   
+           }
+  })
+}
+
   return (
     <div className="flex px-4 py-4 font-inter">
       <Image src={sideImage} alt="Onboarding Image" className="hidden md:flex w-[35%] max-h-full "/>
@@ -74,13 +94,18 @@ const data = {email, password}
           <div className="py-6 flex flex-col justify-center items-center gap-4">
           <button
               type="submit"
-              className="text-white bg-success400 w-[80%] py-[8px] px-[16px] rounded-[8px] font-sans font-medium"
-              onClick={nextpage}
+              className="text-white bg-success400 w-[100%] py-[8px] px-[16px] rounded-[8px] font-sans font-medium"
+              onClick={submit}
             >
-             <p>Proceed</p>
+             {loading? <div
+                  className="spinner-border animate-spin text-white inline-block w-4 h-4 border-4 rounded-full"
+                  role="status"
+                >
+                  <span className="sr-only">Loading...</span>
+                </div> : <p> Proceed </p>}
             </button>
             <div>Not already an Event Creator? <Link href='/authentication/add-creator' className='text-success400'>Create Account</Link></div>
-            <div className='text-grey400'>By continuing past this page, you acknowledge that you read, and agree to our <Link href='' className='underline text-grey700 '>Terms & Conditions for Eventcreators</Link> and our <Link href='' className='underline text-grey700'>Eventcreators Service Agreement</Link>.</div>
+            <div className='text-grey400 text-[12px]'>By continuing past this page, you acknowledge that you read, and agree to our <Link href='' className='underline text-grey700 '>Terms & Conditions for Eventcreators</Link> and our <Link href='' className='underline text-grey700'>Eventcreators Service Agreement</Link>.</div>
           </div>
         </div>
       </div>
@@ -88,4 +113,4 @@ const data = {email, password}
   );
 }
 
-export default ContactDetails;
+export default EventCreatorLogin;
